e36f6b00e672e63415ebdb51b8639edb8b2a0d0b
Rocio <jenrous26@gmail.com>
2024-06-13 23:30:35 -0500

    conexion establecida

--- src/analysis/mongoDB.py
+++ src/analysis/mongoDB.py
@@ -46,7 +46,7 @@ from pymongo import MongoClient # se importa de la biblioteca pymongo mongoclien
 # analyze_data.to_dict() # lo intenté profe :( 
 # # highest_prices_data = highestPrices.to_dict(orient='records')
 
-# #insertar analisia basico en mongoDB
+# #insertar analisia  en mongoDB
 # id_analysis = insert_document(db, 'high_prices', {'type': 'basic_analysis', 'data': analyze_data })
 # print (f'Basic analysis document inserted with Id: {id_analysis}')
 
@@ -66,30 +66,60 @@ from pymongo import MongoClient # se importa de la biblioteca pymongo mongoclien
 
 # CONEXION A MONGO
 
+# mongo_host = "localhost"
+# mongo_port = "27017"
+# mongo_time = 1000
 
+# mongo_uri = "mongodb://"+mongo_host+ ":" +mongo_port+ "/"
 
-mongo_host = "localhost"
-mongo_port = "27017"
-mongo_time = 1000
-
-mongo_uri = "mongodb://"+mongo_host+ ":" +mongo_port+ "/"
-
-try:
-     client = pymongo.MongoClient(mongo_uri, serverSelectionTimeoutMS = mongo_time)
-     client.server_info()
-     print(f"Conexión exitosa a la base de datos")
-     client.close()
+# try:
+#      client = pymongo.MongoClient(mongo_uri, serverSelectionTimeoutMS = mongo_time)
+#      client.server_info()
+#      print(f"Conexión exitosa a la base de datos")
+#      client.close()
 
      
 
-except pymongo.errors.ServerSelectionTimeoutError as errorTime:
-    print(f"full time" + errorTime) 
-except pymongo.errors.ConnectionFailure as errorConexion:
-     print (f"fail conexion" + errorConexion)
+# except pymongo.errors.ServerSelectionTimeoutError as errorTime:
+#     print(f"full time" + errorTime) 
+# except pymongo.errors.ConnectionFailure as errorConexion:
+#      print (f"fail conexion" + errorConexion)
+
+
+try:
+     client = MongoClient ('localhost', 27017)
+     db = client['products']
+     collection= db ['products_analysis']
+     documents = collection.find()
+     for document in documents:
+          print (document)
+          print("********")
+except Exception as ex:
+    print("error in conexion: {}".format(ex))
+
+
+finally:
+     print("Conexion finalizada exitosamente <3")
+
+# intento de conexion a mongodb: si
+# el almacenamiento fue de mongodb a pytthon.
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
